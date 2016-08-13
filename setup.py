6b9204016ececb838447890cca8877c9b3cfa87a
Allen Tran <realallentran@gmail.com>
2016-08-13 12:32:21 -0700

    setup for library

---
+++ setup.py
@@ -0,0 +1,24 @@
+__author__ = 'allentran'
+
+from setuptools import find_packages, setup
+
+
+if __name__ == '__main__':
+    name = 'vae_seq'
+    setup(
+        name=name,
+        version="0.0.0",
+        author='Allen Tran',
+        author_email='realallentran@gmail.com',
+        description='Complete short sequences using a VAE',
+        packages=find_packages(),
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Programming Language :: Python',
+            'Operating System :: Unix',
+            'Operating System :: MacOS',
+        ],
+        setup_requires=[
+            'setuptools>=3.4.4',
+        ],
+    )
