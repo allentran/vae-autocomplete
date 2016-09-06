aa795620120e30cb2093cb707928704b367fc958
Allen Tran <realallentran@gmail.com>
2016-09-05 20:10:53 -0700

    paper code + data

---
+++ paper/completer.py
@@ -0,0 +1,16 @@
+
+
+import numpy as np
+from vae_seq.training.autocomplete import AutocompleteVAE
+
+
+def main():
+
+    sequences = np.loadtxt('paper/seqs.npy')
+    meta = np.loadtxt('paper/metas.npy')
+
+    vae = AutocompleteVAE(sequences, meta)
+    vae.fit(epochs=30)
+
+if __name__ == "__main__":
+    main()
