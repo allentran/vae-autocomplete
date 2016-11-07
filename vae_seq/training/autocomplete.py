8dc54427622e16014f6b1f23e0d0f4c3b7ab48b7
Mingyu Lu <mingyu.lu@ds-offline4.local.lan>
2016-11-07 11:17:20 -0800

    prediction wrappers

--- vae_seq/training/autocomplete.py
+++ vae_seq/training/autocomplete.py
@@ -18,21 +18,38 @@ class AutocompleteVAE(object):
         if standardize_meta:
             self.meta_means = np.mean(meta, axis=0, keepdims=True)
             self.meta_stds = np.std(meta, axis=0, keepdims=True)
-            self.meta = (meta - self.meta_means) / self.meta_stds
+            self.meta_stds[self.meta_stds == 0] = 1e-10
+            self.meta = self._standardize_meta(meta)
         else:
             self.meta = meta
 
         self.standardize_meta = standardize_meta
-        self.X = np.log(array[:, 1:]) - np.log(array[:, 0:-1])
-        drop_mask = (~np.isfinite(self.X)).sum(axis=1) > 0
-        self.X = self.X[~drop_mask, :]
-        self.meta = self.meta[~drop_mask, :]
-        self.n_seq = self.X.shape[1]
+        self.diff = diff
 
-        self.model = vae.EncoderDecodeCompleterModel(self.n_seq, 1, self.meta.shape[1], depth=3)
+        if diff:
+            self.X, self.meta = self._log_diff(array, self.meta)
+        else:
+            self.X = array
+
+        self.n_seq = self.X.shape[1]
+        self.max_cut = self.X.shape[1]
+        self.min_cut = self.max_cut / 2
+
+        self.model = vae.EncoderDecodeCompleterModel(self.X.shape[1], 1, self.meta.shape[1], depth=3)
 
     def split_data(self, test_frac=0.2, stratify=None):
         return train_test_split(self.X, self.meta, test_size=test_frac, stratify=stratify)
+
+    def _standardize_meta(self, meta):
+        return (meta - self.meta_means) / self.meta_stds
+
+    def _log_diff(self, array, meta, drop = True):
+        X = np.log(array[:, 1:]) - np.log(array[:, 0:-1])
+        if drop:
+            drop_mask = (~np.isfinite(X)).sum(axis=1) > 0
+            X = X[~drop_mask, :]
+            meta = meta[~drop_mask, :]
+        return X, meta
 
     @staticmethod
     def iterate_minibatches(inputs, meta, batchsize, shuffle=True):
@@ -46,6 +63,34 @@ class AutocompleteVAE(object):
             else:
                 excerpt = slice(start_idx, start_idx + batchsize)
             yield inputs[excerpt], meta[excerpt]
+
+    def predict(self, X, meta, filled_length):
+        if self.standardize_meta:
+            meta = self._standardize_meta(meta)
+
+        if self.diff:
+            X, meta = self._log_diff(X, meta, drop=False)
+
+        if filled_length <= 0:
+            X[:, 0] = np.log(1) - np.log(1)
+
+        result = self.model.predict(
+            X[:, :, None],
+            np.repeat(meta[:, None, :], self.n_seq, axis=1)
+        )
+
+        finite_index = max(
+            [i for i in range(self.n_seq) if all(np.isfinite(result[0][:, i, :].flatten()))]
+        )
+
+        return result[0][:, finite_index, :], result[1][:, finite_index, :]
+
+    def _cut(self, x, force_cut=None):
+        if not force_cut:
+            cut = np.random.randint(self.min_cut, self.max_cut)
+        else:
+            cut = force_cut
+        return x.copy()[:, :cut], cut
 
     def fit(self, epochs=None, epochs_since_best=10, batchsize=100):
 
